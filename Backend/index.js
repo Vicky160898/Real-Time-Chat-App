7d702f97deba58dbd451c5d6d95fbe326eca2a0b
vinayak hiwase <vinayakhiwse8@gmail.com>
2023-03-20 00:46:28 +0530

    f

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const UserRouter = require("./routes/userRoutes");
 const connect = require("./config/db");
 const ChatUser = require("./routes/chatRoutes");
+const MessageRouter = require("./routes/messageRoutes");
 const colors = require("colors");
 const { notFound } = require("./middleware/errorMiddleware");
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json()); //to accept json  data
 app.use("/api/user", UserRouter);
 app.use("/api/chat", ChatUser);
+app.use("/api/message", MessageRouter);
 app.use(notFound);
 connect();
 app.listen(PORT, () => console.log("server started".yellow.bold));
